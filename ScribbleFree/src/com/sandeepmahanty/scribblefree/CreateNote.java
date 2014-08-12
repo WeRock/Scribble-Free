e8d20e910d2e1aba7753aaa9bbfca7fc26fc1188
Sandeep Mahanty <sandeepmahanty@hotmail.com>
2014-08-13 00:14:43 +0530

    added temporary solution for left and right indentation.

--- ScribbleFree/src/com/sandeepmahanty/scribblefree/CreateNote.java
+++ ScribbleFree/src/com/sandeepmahanty/scribblefree/CreateNote.java
@@ -1,3 +1,6 @@
+/*
+ * 
+ */
 package com.sandeepmahanty.scribblefree;
 
 import java.util.Calendar;
@@ -9,20 +12,35 @@ import android.util.Log;
 import android.view.KeyEvent;
 import android.view.View;
 import android.view.View.OnKeyListener;
-import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageButton;
 import android.widget.Toast;
-
 import com.sandeepmahanty.scribblefree.adapter.ScribbletsDBAdapter;
 import com.sandeepmahanty.scribblefree.model.HtmlElements;
 
+/**
+ * The Class CreateNote.
+ */
 public class CreateNote extends Activity {
 	
+	/** The title. */
 	private EditText title;
+	
+	/** The content. */
 	private EditText content;
+	
+	/** The db helper. */
 	private ScribbletsDBAdapter dbHelper;
 	
+	/** The is list mode. */
 	private static boolean isListMode=false; 
+	
+	/** The indent level. */
+	private static int INDENT_LEVEL=0;
+	
+	/* (non-Javadoc)
+	 * @see android.app.Activity#onCreate(android.os.Bundle)
+	 */
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		
@@ -35,25 +53,62 @@ public class CreateNote extends Activity {
 			
 			@Override
 			public boolean onKey(View view, int keyCode, KeyEvent keyEvent) {
-				if(keyCode == KeyEvent.KEYCODE_ENTER){
-					if(isListMode){
-						String text=content.getText().toString();
-						Log.d("After Pressing enter: ", text);
-						text+=Html.fromHtml("<br/>")+" "+Html.fromHtml(HtmlElements.BULLET_TYPE_FILLED)+" ";
-						content.setText(text);
-						content.setSelection(content.getText().length());						
-					}					
+				
+				if (keyEvent.getAction()!=KeyEvent.ACTION_DOWN)
+                    return true;
+			
+				switch(keyCode){
+				case KeyEvent.KEYCODE_ENTER : if(isListMode){
+												
+													addHtmlElement(HtmlElements.BULLET_TYPE_FILLED);
+													return true;
+											}
+											return false;
+				case KeyEvent.KEYCODE_BACK: 
+					finish();
+					Log.d("Back key Called", "Called");
+					return true;
+				default:return false;
 				}
-				return false;
+				
 			}
 		});
 	}
 	
+	/**
+	 * Adds the html element.
+	 *
+	 * @param element the element
+	 */
+	private void addHtmlElement(String element){
+		
+		String text=content.getText().toString();
+		if(element.equals(HtmlElements.BULLET_TYPE_FILLED) || element.equals(HtmlElements.BULLET_TYPE_HOLLOW)){
+			text+=Html.fromHtml("<br/>");
+		}
+		
+		text+=" "+Html.fromHtml(element)+Html.fromHtml(HtmlElements.ELEMENT_TYPE_TAB);
+		content.setText(text);
+		content.setSelection(content.getText().length());
+		
+	}
 	
+	/* (non-Javadoc)
+	 * @see android.app.Activity#onPause()
+	 */
 	@Override
 	protected void onPause() {
 			
 		super.onPause();
+		saveNote();
+		
+	}
+	
+	/**
+	 * Save note.
+	 */
+	private void saveNote(){
+		
 		dbHelper = new ScribbletsDBAdapter(getApplicationContext());
         dbHelper.open();
 		Calendar date= Calendar.getInstance();
@@ -82,19 +137,60 @@ public class CreateNote extends Activity {
 		
 	}
 	
+	/**
+	 * Adds the bullet.
+	 *
+	 * @param view the view
+	 */
 	public void addBullet(View view){
 		if(isListMode){
 			isListMode=false;
-			((Button)view).setText("Bullet Mode");
+			((ImageButton)view).setImageResource(R.drawable.ic_bullet_view);
 			
 		}else{			
 			isListMode= true;
-			String text=content.getText().toString();
-			text+=Html.fromHtml("<br/>")+" "+Html.fromHtml(HtmlElements.BULLET_TYPE_FILLED)+" ";
-			content.setText(text);
-			content.setSelection(content.getText().length());
-			((Button)view).setText("List Mode");
+			INDENT_LEVEL=1;
+			addHtmlElement(HtmlElements.BULLET_TYPE_FILLED);			
+			((ImageButton)view).setImageResource(R.drawable.ic_list_view);
 		}
 		
+	}
+	
+	/**
+	 * Adds the right indent.
+	 *
+	 * @param view the view
+	 */
+	public void addRightIndent(View view){
+		if(INDENT_LEVEL<4){
+			String bullet=Html.fromHtml(HtmlElements.BULLET_TYPE_FILLED).toString();
+			String text= content.getText().toString();
+			text=text.substring(0, text.lastIndexOf(bullet));
+			text+="   "+Html.fromHtml(HtmlElements.BULLET_TYPE_FILLED);
+			content.setText(text);
+			content.setSelection(content.getText().length());
+			INDENT_LEVEL+=1;
+			if(INDENT_LEVEL>4)
+				INDENT_LEVEL=4;
+		}			
+	}
+	
+	/**
+	 * Adds the left indent.
+	 *
+	 * @param view the view
+	 */
+	public void addLeftIndent(View view){
+		if(INDENT_LEVEL>0){
+			String bullet=Html.fromHtml(HtmlElements.BULLET_TYPE_FILLED).toString();
+			String text= content.getText().toString();
+			text=text.substring(0, text.lastIndexOf(bullet)-4);
+			text+=Html.fromHtml(HtmlElements.ELEMENT_TYPE_TAB)+" "+Html.fromHtml(HtmlElements.BULLET_TYPE_FILLED);
+			content.setText(text);
+			content.setSelection(content.getText().length());
+			INDENT_LEVEL-=1;
+			if(INDENT_LEVEL<0)
+				INDENT_LEVEL=0;
+		}
 	}
 }
--- ScribbleFree/src/com/sandeepmahanty/scribblefree/model/HtmlElements.java
+++ ScribbleFree/src/com/sandeepmahanty/scribblefree/model/HtmlElements.java
@@ -2,6 +2,9 @@ package com.sandeepmahanty.scribblefree.model;
 
 public class HtmlElements {
 	
+	public static final String ELEMENT_TYPE_TAB="&#9;";
 	public static final String BULLET_TYPE_FILLED="&#8226;";
-
+	public static final String BULLET_TYPE_HOLLOW="&#959;";
+	public static final String BULLET_TYPE_SQUARE="&#9830;";
+	
 }
